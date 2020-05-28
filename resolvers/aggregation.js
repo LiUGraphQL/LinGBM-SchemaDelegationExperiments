8e307f0573c5f836064fbb1531cb89826772559e
Samuel Barnholdt <samuel.barnholdt@gmail.com>
2020-05-28 10:11:34 +0200

    aggregation

--- resolvers/aggregation.js
+++ resolvers/aggregation.js
@@ -1,7 +1,6 @@
 export const AggregateUgStudents = (schema) => {
   return {
     age: async (parent, args, context, info) => {
-      console.log(parent);
       return {
         avg: parent.avg.age,
         min: parent.min.age,
